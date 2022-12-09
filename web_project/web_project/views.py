6b82595d481db20f71a58b89f1aea80e8525128a
agungadhika <agungadhika@gmail.com>
2022-12-10 02:42:11 +0800

    ngantuk euy

---
+++ web_project/web_project/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+    return render(request, 'home/index.html')
+    # return HttpResponse("Home")
+
+# def documentation(request):
+    # return HttpResponse("Documentation")
