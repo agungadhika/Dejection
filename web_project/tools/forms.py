156023a8d346428ddfb42d9a8ce260b312c354c8
agungadhika <agungadhika@gmail.com>
2022-12-21 01:54:39 +0800

    styling 90%, kurang button. next crud

---
+++ web_project/tools/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class FormField(forms.Form):
+    url_field = forms.URLField(label="Input Url")
+    vuln = (
+        ('xss', 'Cros-site Scripting'),
+        ('sqli', 'SQL Injection'),
+        ('command', 'Command Injection'),
+    )
+    type_attack = forms.ChoiceField(choices = vuln)
+    
+
+    # integer_field = forms.IntegerField()
--- web_project/tools/views.py
+++ web_project/tools/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .forms import FormField
 # Create your views here.
 def index(request):
-    return render(request, 'tools/index.html')
+    form_field = FormField()
+    
+    context = {
+        'data_form':form_field
+    }
+    print(request.POST)
+    return render(request, 'tools/index.html',context)
