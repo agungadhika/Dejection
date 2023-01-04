55e7e0c2e4cf6584ca958a69a7d7dbb0359462f6
Agung <34364395+Agung-Krisna@users.noreply.github.com>
2023-01-04 23:51:11 +0800

    added more functionalities to views

--- web_project/tools/views.py
+++ web_project/tools/views.py
@@ -42,7 +42,6 @@ def index(request):
         if (form_field.is_valid()):
             url = form_field.cleaned_data["url_field"]
             type_attack = form_field.cleaned_data['type_attack']
-            print(type_attack)
             if(type_attack == 'xss'):
                 # print(xss.objects.values_list("payload"))
                 # payload = xss.objects.values("payload").get(id=1)['payload']
@@ -57,7 +56,7 @@ def index(request):
                 # print(submit)
                 # payload = xss.objects.all()
             elif(type_attack == "sqli"):
-                for i, p in enumerate(sqlinjection.objects.all()):
+                for i, p in enumerate(sqlinjection.objects.all()[:10]):
                     payload = p.payload
                     submit = re.sub("\$[a-zA-Z0-9]*\$", payload, url)
                     submit = requote_uri(submit)
@@ -65,8 +64,6 @@ def index(request):
                     response = session.post(submit)
                     status_code = response.status_code
                     content_length = len(response.text)
-                    if (i == 4 or i == 5):
-                        print(i, submit, response.text, "\n\n\n\n")
                     result.append([payload, status_code, content_length])
                 # payload = sqlinjection.objects.all()
             elif(type_attack == "nosql"):
@@ -102,5 +99,4 @@ def index(request):
 
 def createResult(request):
     # return render(request, "tools/tools.html", {"response_code": response_code, "content_length": content_length, "payload": payload})
-    print(result)
     return render(request, "tools/beta_tools.html", {"result": result})
