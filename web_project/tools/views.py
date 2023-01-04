7f9c2f7bcb1071914e052f9a57c894ada9d7cc66
Agung <34364395+Agung-Krisna@users.noreply.github.com>
2023-01-04 23:35:00 +0800

    added beta templates and views

--- web_project/tools/views.py
+++ web_project/tools/views.py
@@ -5,7 +5,8 @@ from .forms import FormField
 import requests
 import os
 import re
-
+from requests.utils import requote_uri
+from bs4 import BeautifulSoup as bs
 # Create your views here.
 # def index(request):
 #     form_field = FormField()
@@ -15,14 +16,27 @@ import re
 #     }
 #     print(request.POST)
 #     return render(request, 'tools/index.html',context)
-response_code = ""
-content_length = ""
+response_code = []
+content_length = []
 payload = []
+result = []
+
+URL = "http://47.254.202.210/login.php"
+
+def createSession(url):
+    s = requests.Session()
+    soup = bs(s.get(url).content, "html.parser")
+    hidden = soup.find("input", type="hidden")
+    token = hidden.get("value")
+    payload = {"username": "admin", "password": "password", "Login":"Login", "user_token": token}
+    s.post(url, data=payload)
+    return s
 
 def index(request):
     global response_code
     global content_length
     global payload
+    global result
     if request.method == "POST":
         form_field = FormField(request.POST)
         if (form_field.is_valid()):
@@ -35,22 +49,38 @@ def index(request):
                 for p in xss.objects.all():
                     payload = p.payload
                     submit = re.sub("\$[a-zA-Z0-9]*\$", payload, url)
-                payload = xss.objects.all()
+                    response = requests.post(quote(submit))
+                    status_code = requests.post(submit).status_code
+                    content_length = len(response.content)
+                    result.append([payload, status_code, content_length])
+                # payload = xss.objects.all()
                 # print(submit)
             elif(type_attack == "sqli"):
-                for p in sqlinjection.objects.all():
+                for i, p in enumerate(sqlinjection.objects.all()):
                     payload = p.payload
                     submit = re.sub("\$[a-zA-Z0-9]*\$", payload, url)
-                payload = sqlinjection.objects.all()
+                    submit = requote_uri(submit)
+                    session = createSession(URL)
+                    response = session.post(submit)
+                    status_code = response.status_code
+                    content_length = len(response.text)
+                    if (i == 4 or i == 5):
+                        print(i, submit, response.text, "\n\n\n\n")
+                    result.append([payload, status_code, content_length])
+                # payload = sqlinjection.objects.all()
             elif(type_attack == "nosql"):
                 for p in nosqlinjection.objects.all():
                     payload = p.payload
                     submit = re.sub("\$[a-zA-Z0-9]*\$", payload, url)
-                payload = nosqlinjection.objects.all()
+                    response = requests.post(submit)
+                    status_code = requests.post(submit).status_code
+                    content_length = len(response.content)
+                    result.append([payload, status_code, content_length])
+                # payload = nosqlinjection.objects.all()
             # result = requests.post(submit, json={"username":"hello", "password":"world"})
-            response = requests.post(submit) 
-            response_code  = response.status_code
-            content_length = len(response.content)
+            # response = requests.post(submit) 
+            # response_code  = response.status_code
+            # content_length = len(response.content)
             return HRR("result/")
     else:
         form_field = FormField()
@@ -60,4 +90,6 @@ def index(request):
     return render(request, 'tools/index.html', context)
 
 def createResult(request):
-    return render(request, "tools/tools.html", {"response_code": response_code, "content_length": content_length, "payload": payload})
+    # return render(request, "tools/tools.html", {"response_code": response_code, "content_length": content_length, "payload": payload})
+    print(result)
+    return render(request, "tools/beta_tools.html", {"result": result})
