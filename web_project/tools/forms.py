6892210f17fb29348b892e09d67816fa5725f88f
agungadhika <agungadhika@gmail.com>
2023-04-27 23:06:38 +0800

    tampilan home, tools done. next documentation, post method and result

--- web_project/tools/forms.py
+++ web_project/tools/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class FormField(forms.Form):
-    url_field = forms.URLField(label="Input Url")
+    url_field = forms.URLField(label="Input URL", label_suffix=None)
     vuln = (
         ('xss', 'Cros-site Scripting'),
         ('sqli', 'SQL Injection'),
@@ -10,10 +10,20 @@ class FormField(forms.Form):
         ('nosql', 'NoSQL Injection'),
     )
     type_attack = forms.ChoiceField(choices = vuln)
+
     CHOICES = [
         ("GET", "GET Method"),
         ("POST", "POST Method")
     ]
     attack_method = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
 
+    def __init__(self, *args, **kwargs):
+        super(FormField, self).__init__(*args, **kwargs)
+        self.fields['url_field'].label = 'Input URL'  # Atur ulang label field
+        # self.fields['url_field'].widget.attrs.update({'style': 'font-size: 12px'})
+        # self.fields['url_field'].label_suffix = None  # Hilangkan tanda titik dua (:)
+        self.fields['url_field'].widget.attrs.update({'class': 'form-control form-control-lg'})
+        self.fields['url_field'].widget.attrs.update({'placeholder': 'Masukkan URL'})
+        self.fields['type_attack'].widget.attrs.update({'class': 'form-control'})
+        # self.fields['attack_method'].widget.attrs.update({'class': 'form-check',})
     # integer_field = forms.IntegerField()
