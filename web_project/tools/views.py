4e1a91ce4c0eb131ebb667d96518eb79f6b615a3
kaorikizuna <34364395+Agung-Krisna@users.noreply.github.com>
2023-07-11 00:03:24 +0800

    added more support for post method in NoSQL Injection

--- web_project/tools/views.py
+++ web_project/tools/views.py
@@ -4,19 +4,19 @@ from .models import xss, commandinjection, sqlinjection, xxeinjection, nosqlinje
 from .forms import FormField
 from .utils.get_request import get_request
 from threading import Thread
-from .utils.post_request import post_validation, post_request
+# from .utils.post_request import post_validation, post_request
 from bs4 import BeautifulSoup as bs
 import re
 from requests.utils import requote_uri
 import requests
-from .utils.post_request import post_validation, post_request, URL
+from .utils.post_request import post_validation, post_request, URL, post_request_nosql
 
 # post method multiprocessing
 # login DVWA and do post and get request there
 
 results = []
 threads = None
-login_dvwa = True
+login_dvwa = True # change to true when checking Bwapp
 
 URL = URL
 host_up = True
@@ -50,12 +50,13 @@ def get_view(url: str, type_attack: str):
 
 def postMethodValidation(request):
     global threads
-    # login_dvwa = False 
     url = request.session["url"]
     type_attack = request.session["type_attack"]
     context_data = post_validation(url, type_attack, login_dvwa = login_dvwa)
     
     if (request.method == "GET"):
+        if(type_attack == "nosql"):
+            return render(request, "tools/nosql_post_method.html")
         return render(request, "tools/post_method.html", context=context_data)
     else:
         data = request.POST.dict()
@@ -86,7 +87,7 @@ def postMethodView(url, type_attack, data):
     elif(type_attack == "command"):
         result = post_request(url, deleteEmptyString(list(commandinjection.objects.values_list('payload', flat=True))), data, login_dvwa = login_dvwa)
     elif(type_attack == "nosql"):
-        result = post_request(url, deleteEmptyString(list(nosqlinjection.objects.values_list('payload', flat=True))), data, login_dvwa = login_dvwa, json_encode = True)
+        result = post_request_nosql(url=url, data=data, payloads=list(nosqlinjection.objects.values_list('payload', flat=True)), login_dvwa=login_dvwa, json_encode = True)
     
     results = result
 
@@ -124,3 +125,15 @@ def loading_info(request):
     if (host_up == False):
         return HttpResponse("host is down")
     return HttpResponse(threads.is_alive())
+
+
+"""
+no sql
+kita perlu satu input yang blank,
+input menerima string, tanpa encode langsung dikirim.
+
+Saat post validation, buat sebuah checkmark yang menandakan apakah 
+input/select list akan dibypass atau tidak.
+
+
+"""
